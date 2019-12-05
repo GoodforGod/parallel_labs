d38789715495461b0e57b4db633a4abbf087707e
Anton Kurako <goodforgod.dev@gmail.com>
2019-12-05 18:00:57 +0300

    Improved echo for final script

--- lab1/runner_final.sh
+++ lab1/runner_final.sh
@@ -12,9 +12,11 @@ do
         N=$(($1 + i * $2))
         millis=$(./${lab} ${N} | tail -1 | grep -Eo "[0-9]+$")
         runs+=(${millis})
-        echo "Seq $i run completed in $millis millis for N $N."
+        #echo "Seq $i run completed in $millis millis for N $N."
     done
 
+    last=$(($1 + 10 * $2))
+    echo "$lab: From '$1' To '$last' with '$2' Step"
     echo "ALL: ${runs[@]}"
     sum=$(echo "${runs[@]/%/+} 0" | bc)
     avg=$((sum / 10))
