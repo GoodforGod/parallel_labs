6ed72fd9ce855b176138395f4b72b4fcb5dcb12f
Anton Kurako <goodforgod.dev@gmail.com>
2019-12-09 12:55:36 +0300

    Improved runner script with averaged data

--- lab1/runner_final.sh
+++ lab1/runner_final.sh
@@ -1,31 +1,41 @@
 #!/bin/bash
 
-runs=()
+# ./runner_final.sh 400 880
+# N1 - 400
+# N2 - 9200
+# STEPS - 10
+# STEP - (9200 - 400) / 10 = 880
 
 labs=(lab1-seq lab1-par-2 lab1-par-4 lab1-par-6 lab1-par-8)
-#, lab1-seq, lab1-seq, lab1-seq)
 
-# N1 - 300
-# N2 - 7400
-# STEP - 710
-# STEPS - 10
+runs=()
 
 for lab in ${labs[@]}
 do
     for i in {0..10}
     do
+        iteration=()
         N=$(($1 + i * $2))
-        millis=$(./${lab} ${N} | tail -1 | grep -Eo "[0-9]+$")
-        runs+=(${millis})
+        for run in {1..10}
+        do
+            millis=$(./${lab} ${N} | tail -1 | grep -Eo "[0-9]+$")
+            iteration+=(${millis})
+        done
+
+        sum=$(echo "${iteration[@]/%/+} 0" | bc)
+        avg=$((sum / 10))
+#        echo "$lab: All: ${iteration[@]} for i: $i"
+#        echo "$lab: Average: $avg for i: $i"
+        runs+=(${avg})
         #echo "Seq $i run completed in $millis millis for N $N."
     done
 
     last=$(($1 + 10 * $2))
     echo "$lab: From '$1' To '$last' with '$2' Step"
-    echo "ALL: ${runs[@]}"
+    echo "$lab: ALL: ${runs[@]}"
     sum=$(echo "${runs[@]/%/+} 0" | bc)
     avg=$((sum / 10))
     runs=()
-    echo "AVG: $avg"
+    echo "$lab: AVG: $avg"
+    echo '-------------------------------------------'
 done
-
--- lab2/runner_final.sh
+++ lab2/runner_final.sh
@@ -1,34 +1,42 @@
 #!/bin/bash
 
-runs=()
+# ./runner_final.sh 400 880
+# N1 - 400
+# N2 - 9200
+# STEPS - 10
+# STEP - (9200 - 400) / 10 = 880
 
 labs=(lab2-amd)
-#, lab1-seq, lab1-seq, lab1-seq)
 
-# N1 - 300
-# N2 - 7400
-# STEP - 710
-# STEPS - 10
+runs=()
 
 for lab in ${labs[@]}
 do
-    for threads in {1..6}
+    for i in {0..10}
     do
-        for i in {0..10}
+        iteration=()
+        N=$(($1 + i * $2))
+        for run in {1..10}
         do
-            N=$(($1 + i * $2))
-            millis=$(./${lab} ${N} ${threads} | tail -1 | grep -Eo "[0-9]+$")
-            runs+=(${millis})
-            #echo "Seq $i run completed in $millis millis for N $N."
+            millis=$(./${lab} ${N} | tail -1 | grep -Eo "[0-9]+$")
+            iteration+=(${millis})
         done
 
-        last=$(($1 + 10 * $2))
-        echo "$lab: From '$1', To '$last', with '$2' Step, for ${threads} framewave threads"
-        echo "ALL: ${runs[@]}"
-        sum=$(echo "${runs[@]/%/+} 0" | bc)
+        sum=$(echo "${iteration[@]/%/+} 0" | bc)
         avg=$((sum / 10))
-        runs=()
-        echo "AVG: $avg"
+#        echo "$lab: All: ${iteration[@]} for i: $i"
+#        echo "$lab: Average: $avg for i: $i"
+        runs+=(${avg})
+        #echo "Seq $i run completed in $millis millis for N $N."
     done
+
+    last=$(($1 + 10 * $2))
+    echo "$lab: From '$1' To '$last' with '$2' Step"
+    echo "$lab: ALL: ${runs[@]}"
+    sum=$(echo "${runs[@]/%/+} 0" | bc)
+    avg=$((sum / 10))
+    runs=()
+    echo "$lab: AVG: $avg"
+    echo '-------------------------------------------'
 done
 
