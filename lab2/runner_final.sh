923e8e182aca8b633932732afd4cf9c6289f9935
Anton Kurako <goodforgod.dev@gmail.com>
2019-12-09 15:50:27 +0300

    Lab 2 runner with thread run added

--- lab2/runner_final.sh
+++ lab2/runner_final.sh
@@ -12,31 +12,33 @@ runs=()
 
 for lab in ${labs[@]}
 do
-    for i in {0..10}
+    for threads in {1..6}
     do
-        iteration=()
-        N=$(($1 + i * $2))
-        for run in {1..10}
+        for i in {0..10}
         do
-            millis=$(./${lab} ${N} | tail -1 | grep -Eo "[0-9]+$")
-            iteration+=(${millis})
+            iteration=()
+            N=$(($1 + i * $2))
+            for run in {1..10}
+            do
+                millis=$(./${lab} ${N} ${threads} | tail -1 | grep -Eo "[0-9]+$")
+                iteration+=(${millis})
+            done
+
+            sum=$(echo "${iteration[@]/%/+} 0" | bc)
+            avg=$((sum / 10))
+            runs+=(${avg})
+#            echo "$lab: All: ${iteration[@]} for i: $i, threads: $threads"
+#            echo "$lab: Average: $avg for i: $i, threads: $threads"
         done
 
-        sum=$(echo "${iteration[@]/%/+} 0" | bc)
+        last=$(($1 + 10 * $2))
+        echo "$lab: From '$1' To '$last' with '$2' Step with $threads threads"
+        echo "$lab: ALL: ${runs[@]}"
+        sum=$(echo "${runs[@]/%/+} 0" | bc)
         avg=$((sum / 10))
-#        echo "$lab: All: ${iteration[@]} for i: $i"
-#        echo "$lab: Average: $avg for i: $i"
-        runs+=(${avg})
-        #echo "Seq $i run completed in $millis millis for N $N."
+        runs=()
+        echo "$lab: AVG: $avg"
+        echo '-------------------------------------------'
     done
-
-    last=$(($1 + 10 * $2))
-    echo "$lab: From '$1' To '$last' with '$2' Step"
-    echo "$lab: ALL: ${runs[@]}"
-    sum=$(echo "${runs[@]/%/+} 0" | bc)
-    avg=$((sum / 10))
-    runs=()
-    echo "$lab: AVG: $avg"
-    echo '-------------------------------------------'
 done
 
